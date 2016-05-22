6c2813ed7a68bcadd8644b9896f285c50198a6a2
Wilson <l7960261@gmail>
2016-05-22 14:59:52 +0800

    add SlackAttribute

--- JSNLog/WebAPI/App_Start/WebApiConfig.cs
+++ JSNLog/WebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebAPI.Filters;
 
 namespace WebAPI
 {
@@ -19,6 +20,8 @@ namespace WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Filters.Add(new SlackAttribute());
         }
     }
 }
---
+++ JSNLog/WebAPI/Filters/SlackAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http.Filters;
+
+namespace WebAPI.Filters
+{
+   internal sealed class SlackAttribute: ExceptionFilterAttribute
+   {
+      public override void OnException(HttpActionExecutedContext actionExecutedContext) {
+         if (actionExecutedContext.Exception != null) {
+            // progress Exception & call slack web api
+         }
+
+         base.OnException(actionExecutedContext);
+      }
+   }
+}
